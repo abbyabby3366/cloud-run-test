58947d62ae9410167a5cd34f0239a262adb4771b
Desmond Giam <desmondgiam@gmail.com>
2026-02-03 22:02:46 +0800

    2nd version

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 8080;
 
+// Function to serve index.html with injected secret
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  const indexPath = path.join(__dirname, 'index.html');
+  let content = fs.readFileSync(indexPath, 'utf8');
+  
+  // Replace placeholder with secret from .env
+  const secret = process.env.SECRET_PHRASE || "No secret found";
+  content = content.replace('{{SECRET_PHRASE}}', secret);
+  
+  res.send(content);
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'about.html'));
 });
 
 app.listen(port, () => {
